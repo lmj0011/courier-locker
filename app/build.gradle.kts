28b3e9912d790962112d432294b6b75d1c773536
lmj0011 <9396189+lmj0011@users.noreply.github.com>
2021-06-21 16:53:39 -0500

    - migrate to Kotlin DSL

---
+++ app/build.gradle.kts
@@ -0,0 +1,273 @@
+import java.io.ByteArrayOutputStream
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.TimeZone
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+val kotlinVersionProp: String by project
+val navigationVersionProp: String by project
+val fragmentVersionProp: String by project
+val roomVersionProp: String by project
+val lifecycleVersionProp: String by project
+val coroutineVersionProp: String by project
+val pagingVersionProp: String by project
+val fuelVersionProp: String by project
+val googleMapsApiKeyDebugProp: String by project
+val googleMapsApiKeyReleaseProp: String by project
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-android-extensions")
+    id("kotlin-kapt")
+    id("androidx.navigation.safeargs.kotlin")
+    id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
+}
+
+android {
+    compileSdk = 30
+    ndkVersion = "21.3.6528147"
+
+    defaultConfig {
+        applicationId = "name.lmj0011.courierlocker"
+        minSdk = 26
+        targetSdk =30
+        versionCode = getCommitCount().toInt()
+        versionName = "2.2.0-beta08"
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+
+        multiDexEnabled = true
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        // ref: https://stackoverflow.com/a/48674264/2445763
+        kapt {
+            arguments {
+                arg("room.schemaLocation", "$projectDir/schemas")
+            }
+        }
+    }
+
+    buildTypes {
+        named("release") {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            isDebuggable = false
+            resValue("bool", "DEBUG_MODE", "false")
+            resValue("string", "google_maps_key", googleMapsApiKeyReleaseProp)
+            setProguardFiles(listOf(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"))
+            manifestPlaceholders["usesCleartextTraffic"] = "false"
+            ndk {
+                debugSymbolLevel = "FULL"
+            }
+        }
+
+        named("debug") {
+            isMinifyEnabled = false
+            isShrinkResources = false
+            isDebuggable = true
+            resValue("bool", "DEBUG_MODE", "true")
+            resValue("string", "google_maps_key", googleMapsApiKeyDebugProp)
+            setProguardFiles(listOf(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"))
+            manifestPlaceholders["usesCleartextTraffic"] = "true"
+        }
+    }
+
+    buildTypes.forEach {
+        it.resValue("string", "app_build", getGitSha().take(8))
+
+        it.resValue("string", "git_commit_count", getCommitCount())
+        it.resValue("string", "git_commit_sha", getGitSha())
+        it.resValue("string", "app_buildtime", getBuildTime())
+    }
+
+    flavorDimensions("default")
+
+    productFlavors {
+        create("dev") { // active development/testing
+            dimension = "default"
+            applicationIdSuffix = ".dev"
+            resValue("string", "app_name", "Courier Locker (dev)")
+            manifestPlaceholders["appIcon"] = "@mipmap/ic_launcher_dev"
+            manifestPlaceholders["appRoundIcon"] = "@mipmap/ic_launcher_dev_round"
+            manifestPlaceholders["appThemeLauncher"] = "@style/AppTheme.NoActionBar"
+        }
+
+        create("prod") { // official release
+            dimension = "default"
+            applicationIdSuffix = ".prod"
+            resValue("string", "app_name", "Courier Locker")
+            manifestPlaceholders["appIcon"] = "@mipmap/ic_launcher"
+            manifestPlaceholders["appRoundIcon"] = "@mipmap/ic_launcher_round"
+            manifestPlaceholders["appThemeLauncher"] = "@style/AppTheme.NoActionBar"
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
+
+    lint {
+        isCheckReleaseBuilds = false
+    }
+
+    // Big Up! to cSn: https://stackoverflow.com/a/36626584/2445763
+    configurations.implementation {
+        isCanBeResolved = true
+    }
+
+    configurations.androidTestImplementation {
+        isCanBeResolved = true
+    }
+
+    project.gradle.addBuildListener(object : BuildListener {
+        override fun settingsEvaluated(settings: Settings) {}
+
+        override fun projectsLoaded(gradle: Gradle) {}
+
+        override fun projectsEvaluated(gradle: Gradle) {}
+
+        override fun buildFinished(result: BuildResult) {
+            var str = "# auto-generated; this file should be checked into version control\n"
+            val resolvedImplementationConfig = configurations.implementation.get().resolvedConfiguration
+            val resolvedAndroidTestImplementationConfig = configurations.androidTestImplementation.get().resolvedConfiguration
+            val fileName = "deps.list.txt"
+            val depsFile = File(projectDir, fileName)
+
+            resolvedImplementationConfig.firstLevelModuleDependencies.forEach { dep ->
+                str += "${dep.moduleGroup}:${dep.moduleName}:${dep.moduleVersion}\n"
+            }
+
+            resolvedAndroidTestImplementationConfig.firstLevelModuleDependencies.forEach { dep ->
+                str += "${dep.moduleGroup}:${dep.moduleName}:${dep.moduleVersion}\n"
+            }
+
+            GlobalScope.launch(Dispatchers.IO) {
+                depsFile.writeText(str)
+                println("\n${fileName} created.\n")
+            }
+        }
+    })
+}
+
+dependencies {
+    val fTree = fileTree("lib")
+    fTree.include("*.jar", "*.aar")
+
+    implementation(fTree)
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersionProp")
+    implementation("androidx.navigation:navigation-fragment-ktx:$navigationVersionProp")
+    implementation("androidx.navigation:navigation-ui-ktx:$navigationVersionProp")
+    implementation("androidx.appcompat:appcompat:1.3.0")
+    implementation("androidx.core:core-ktx:1.5.0")
+    implementation("com.google.android.material:material:1.4.0-beta01")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation("com.jakewharton.timber:timber:4.7.1")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.paging:paging-runtime-ktx:$pagingVersionProp")
+    testImplementation("junit:junit:4.13")
+    androidTestImplementation("androidx.test:runner:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+
+
+    // Room dependencies
+    implementation("androidx.room:room-runtime:$roomVersionProp")
+    annotationProcessor("androidx.room:room-compiler:$roomVersionProp")
+    kapt("androidx.room:room-compiler:$roomVersionProp")
+
+    // GSON
+    implementation("com.google.code.gson:gson:2.8.6")
+
+    // Lifecycle-aware components
+    // ref: https://developer.android.com/jetpack/androidx/releases/lifecycle
+    implementation("androidx.lifecycle:lifecycle-extensions:$lifecycleVersionProp")
+
+    // useful for testing with mock data
+    implementation("com.github.moove-it:fakeit:v0.7")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutineVersionProp")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutineVersionProp")
+
+    // WorkManager
+    implementation("androidx.work:work-runtime-ktx:2.5.0")
+
+    // HTTP lib
+    implementation("com.github.kittinunf.fuel:fuel:$fuelVersionProp")
+    implementation("com.github.kittinunf.fuel:fuel-json:$fuelVersionProp")
+
+    // androidx.preference
+    implementation("androidx.preference:preference-ktx:1.1.1")
+
+    // An adaptation of the JSR-310 backport for Android.
+    implementation("com.jakewharton.threetenabp:threetenabp:1.2.1")
+
+    // Firebase SDK
+    implementation("com.google.firebase:firebase-crashlytics:18.0.0")
+    implementation("com.google.firebase:firebase-analytics:19.0.0")
+
+    // Maps SDK and FusedLocationProviderClient dependencies
+    implementation("com.google.android.gms:play-services-basement:17.6.0")
+    implementation("com.google.android.gms:play-services-base:17.6.0")
+    implementation("com.google.android.gms:play-services-gcm:17.0.0")
+    implementation("com.google.android.gms:play-services-location:18.0.0")
+
+    // Kotson: Gson for Kotlin
+    implementation("com.github.salomonbrys.kotson:kotson:2.5.0")
+
+    // Maps SDK for Android Utility Library
+    // https://developers.google.com/maps/documentation/android-sdk/v3-client-migration#import-the-beta-compatible-utility-library
+    implementation("com.google.android.libraries.maps:maps:3.1.0-beta")
+    implementation("com.google.maps.android:android-maps-utils-v3:1.3.1")
+
+    // dependency injection
+    implementation("org.kodein.di:kodein-di:7.0.0")
+
+    // sqlite
+    implementation("com.github.requery:sqlite-android:3.35.4")
+
+    // https://github.com/Zhuinden/livedata-combinetuple-kt#livedata-combinetuple-kt
+    implementation("com.github.Zhuinden:livedata-combinetuple-kt:1.2.1")
+}
+
+// Git is needed in your system PATH for these commands to work.
+// If it's not installed, you can return a random value as a workaround
+// ref: https://github.com/tachiyomiorg/tachiyomi/blob/master/app/build.gradle.kts
+fun getCommitCount(): String {
+    return runCommand("git rev-list --count HEAD")
+}
+
+fun getGitSha(): String {
+    return runCommand("git rev-parse HEAD")
+}
+
+fun getBuildTime(): String {
+    val df = SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'")
+    df.timeZone = TimeZone.getTimeZone("UTC")
+    return df.format(Date())
+}
+
+fun runCommand(command: String): String {
+    val byteOut = ByteArrayOutputStream()
+    project.exec {
+        commandLine = command.split(" ")
+        standardOutput = byteOut
+    }
+    return String(byteOut.toByteArray()).trim()
+}
---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+
+buildscript {
+    val kotlinVersionProp: String by project
+    val navigationVersionProp: String by project
+
+    repositories {
+        google()
+        mavenCentral()
+        flatDir {
+            dirs("libs")
+        }
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.0-beta04")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersionProp")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navigationVersionProp")
+        classpath("com.google.gms:google-services:4.3.8")  // Google Services plugin
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.7.0")  // Crashlytics plugin
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        jcenter() {
+            content {
+                includeModule("com.android.volley", "volley")
+            }
+        }
+        maven(url = "https://jitpack.io")
+        
+    }
+}
+
+tasks.create("clean") {
+    delete(rootProject.buildDir)
+}
+
+
